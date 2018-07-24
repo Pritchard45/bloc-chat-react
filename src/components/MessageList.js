f70ed40370f082b549e0c2e3c89b41520f78e5c2
Alex <alexpritchard45@gmail.com>
2018-07-24 16:01:00 -0400

    update to check

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -8,28 +8,17 @@ class MessageList extends Component {
       username: "Alex ",
       content: " ",
       sentAt: " ",
-      roomId: " "
     };
     this.messagesRef = this.props.firebase.database().ref('messages');
   }
 
 
   componentDidMount() {
-
     this.messagesRef.on('value', snapshot => {
-      const messageChanges = [];
-      snapshot.forEach((message) => {
-        messageChanges.push({
-          key: message.key,
-          username: message.val().username,
-          content: message.val().content,
-          sentAt: (new Date(message.sentAt)).toLocaleString('en-GB', {timeZone: 'EST'}),
-          roomId: this.state.roomId
-        });
+      this.props.activeRoom
       });
-      this.setState({ messages: messageChanges});
-    });
-  }
+    }
+
 
 /*
   this.messagesRef.on('value', snapshot => {
@@ -55,7 +44,7 @@ class MessageList extends Component {
       username: this.state.username,
       content: e.target.value,
       sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
-      roomId: this.state.roomId
+      roomId: this.props.activeRoom
     });
   }
 
@@ -65,7 +54,7 @@ class MessageList extends Component {
         username: this.state.username,
         content: this.state.content,
         sentAt: this.state.sentAt,
-        roomId: this.state.roomId
+
       });
       this.setState({username: "", content: "", sentAt: ""});
     }
@@ -81,9 +70,9 @@ class MessageList extends Component {
     return (
       <section>
         <h3>Messages</h3>
-        {this.state.messages.map( message => 
+        {this.state.messages.map( message =>
           <li key = {message.key}>
-            <p>{message.roomId}{message.username}{message.content}</p>
+            <p>{message.username}{message.content}</p>
             <p>{message.sentAt}</p>
           </li>
         )}
